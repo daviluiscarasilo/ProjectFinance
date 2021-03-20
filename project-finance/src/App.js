905e534fb84773273b4b1a85b44e6bc9a694cbda
root <root@DESKTOP-D3FK6QP.localdomain>
2021-03-20 13:59:13 -0300

    Realização de imports, e exclusão de arquivos não necessários

--- project-finance/src/App.js
+++ project-finance/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import { FaCloudRain } from 'react-icons/fa'
+import Button from 'react-bootstrap/Button'
+import Toast from 'react-bootstrap/Toast'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Teste testado testando</h1>
     </div>
   );
 }
