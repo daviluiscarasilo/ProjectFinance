cee9a8dfa5319e3fbb0096dbcbc2d5cff12189fd
root <root@DESKTOP-D3FK6QP.localdomain>
2021-03-27 17:18:21 -0300

    Implementação validações e requisições api

--- project-finance/src/App.js
+++ project-finance/src/App.js
@@ -1,107 +1,214 @@
 import './App.css';
 import React, { useState } from 'react'
-
-import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-
 import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-
+import Col from 'react-bootstrap/Col'
 import { MDBContainer } from 'mdbreact';
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import { FaDollarSign } from 'react-icons/fa'
-import { FaRegCalendar } from 'react-icons/fa'
+import { FaRegCalendar, FaSearchDollar, FaDollarSign } from 'react-icons/fa'
+
+import DatePicker from "react-datepicker";
 import Button from 'react-bootstrap/Button'
+import "react-datepicker/dist/react-datepicker.css";
 import Toast from 'react-bootstrap/Toast'
-import Dropdown from 'react-bootstrap/Dropdown'
-
-/**
- * Essa função é responsável por criar o cabeçalho do site
- * @returns Cabeçalho do site
- */
-function Cabecalho() {
-  return (
-    <>
-      <div className="borda-cabecalho" F variant="dark" calss>
-        <h1><FaDollarSign /> Finance Project</h1>
-      </div>
-
-      < Nav variant="light" activeKey="1" justify variant="tabs" defaultActiveKey="/home" className="pesquisa" F variant="dark" calss>
-        <NavDropdown title="Moeda" id="Moeda" >
-          <NavDropdown.Item eventKey="USD-BRL">USD-BRL (Dólar Comercial)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="USDT-BRL">USDT-BRL (Dólar Turismo)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="CAD-BRL">CAD-BRL (Dólar Canadense)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="AUD-BRL">AUD-BRL (Dólar Australiano)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="EUR-BRL">EUR-BRL (Euro)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="GBP-BRL">GBP-BRL (Libra Esterlina)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="ARS-BRL">ARS-BRL (Peso Argentino)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="JPY-BRL">JPY-BRL (Iene Japonês)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="CHF-BRL">CHF-BRL (Franco Suíço)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="CNY-BRL">CNY-BRL (Yuan Chinês)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="YLS-BRL">YLS-BRL (Novo Shekel Israelense)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="BTC-BRL">BTC-BRL (Bitcoin)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="LTC-BRL">LTC-BRL (Litecoin)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="ETH-BRL">ETH-BRL (Ethereum)</NavDropdown.Item>
-          <NavDropdown.Item eventKey="XRP-BRL">XRP-BRL (Ripple)</NavDropdown.Item>
-        </NavDropdown>
-
-        <Form inline className="pesquisa" F variant="dark" calss>
-          <FaRegCalendar /><FormControl type="mm/dd/aaaa" placeholder="Data Inicial" className="mr-sm-2" />
-        </Form>
-
-        <Form inline className="pesquisa" F variant="dark" calss>
-          <FaRegCalendar /><FormControl type="mm/dd/aaaa" placeholder="Data Final" className="mr-sm-2" />
-        </Form>
-
-        <NavDropdown title="Indicador" id="Indicador">
-          <NavDropdown.Item eventKey="bid">Valor de compra</NavDropdown.Item>
-          <NavDropdown.Item eventKey="ask">Valor de venda</NavDropdown.Item>
-          <NavDropdown.Item eventKey="high">Valor Máximo</NavDropdown.Item>
-          <NavDropdown.Item eventKey="low">Valor Mínimo</NavDropdown.Item>
-          <NavDropdown.Item eventKey="pctChange">Variação %</NavDropdown.Item>
-        </NavDropdown>
-
-      </Nav>
-    </>
-  )
-}
-/**
- * Essa função é responsável por criar o rodapé do site
- * @returns Rodapé do site
- */
-function Rodape() {
-  return (
-    <div className="rodape">
-      <MDBContainer className="container-rodape">
-        <table className="tabela-rodape">
-          <tbody>
-            <tr>
-              <td className="align-bottom celula-rodape">
-                <strong> Developed by: Pedro Spinardi and Davi L. Carasilo -- 2021 A Fatec’s project </strong>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </MDBContainer>
-    </div>
-
-  )
-}
-/**
- * Essa função é responsável por criar o corpo do site
- * @returns Corpo do site
- */
-function Corpo() {
-  return (
-    <div className="corpo">
-
-    </div>
-  )
-}
+import moment from 'moment'
 
 
 function App() {
+
+  const [erroValidacaoCampos, setErroValidacaoCampos] = useState("");
+  const [erro, setErro] = useState("");
+  const [dataInicial, setDataInicial] = useState(new Date());
+  const [dataFinal, setDataFinal] = useState(new Date());
+  const [moeda, setMoeda] = useState("");
+  const [moedaTexto, setMoedaTexto] = useState("");
+  const [indicador, setIndicador] = useState("");
+  const [indicadorTexto, setIndicadorTexto] = useState("");
+  const [dadosRelatorio, setDadosRelatorio] = useState({});
+
+  function TimestampParaData(timestamp){
+    var t = new Date( parseInt(timestamp));
+    return t;
+  }
+  function ValidaDadosObrigatorios() {
+    return moeda !== "" && indicador !== "" && dataInicial !== "" && dataFinal !== ""
+  }
+  function ValidarDatas() {
+    return dataInicial < dataFinal
+  }
+  function ValidaCampos() {
+    var status = true;
+    var mensagemErro = "";
+
+    if (!ValidaDadosObrigatorios()) {
+      mensagemErro = "Todos os campos são obrigatórios para a pesquisa;"
+      status &= false;
+    }
+
+    if (!ValidarDatas()) {
+      mensagemErro += "\n Data final não pode ser menor  ou igual  a data inicial;"
+      status &= false;
+    }
+    if (mensagemErro !== "")
+      setErroValidacaoCampos(mensagemErro)
+
+    return status;
+  }
+
+  async function GerarRelatorio() {
+    if (ValidaCampos()) {
+      var UrlFinance = "https://economia.awesomeapi.com.br/json/daily/" +
+        moeda + `/?start_date=${moment(dataInicial).format("yyyyMMDD")}&end_date=${moment(dataFinal).format("yyyyMMDD")}`
+
+      await fetch(UrlFinance)
+        .then(response => response.json())
+        .then(data => {
+          setDadosRelatorio(data)
+        })
+        .catch(function (error) {
+          setErro('Houve um problema na requisição')
+        })
+    }
+  }
+
+
+  /**
+   * Essa função é responsável por criar o cabeçalho do site
+   * @returns Cabeçalho do site
+   */
+  function Cabecalho() {
+    return (
+      <>
+        <div className="borda-cabecalho" variant="dark" >
+          <h1><FaDollarSign /> Finance Project</h1>
+        </div>
+        <Nav variant="light" activeKey="1" justify variant="tabs" defaultActiveKey="/home" className="pesquisa" variant="dark" >
+          <Form inline>
+            <Form.Row className="align-items-center">
+              <Col xs="auto">
+                <label>Moeda</label>
+                <select className="custom-select mr-sm-2" onChange={function (event) {
+                  setMoeda(event.target.value);
+                  setMoedaTexto(event.target.value);
+                }}>
+                  {(moedaTexto &&
+                    <option value="">{moedaTexto}</option>)
+                    ||
+                    <option value="">Escolha uma opção</option>
+
+                  }
+                  <option value="USD-BRL">USD-BRL (Dólar Comercial)</option>
+                  <option value="USDT-BRL">USDT-BRL (Dólar Turismo)</option>
+                  <option value="CAD-BRL">CAD-BRL (Dólar Canadense)</option>
+                  <option value="AUD-BRL">AUD-BRL (Dólar Australiano)</option>
+                  <option value="EUR-BRL">EUR-BRL (Euro)</option>
+                  <option value="GBP-BRL">GBP-BRL (Libra Esterlina)</option>
+                  <option value="ARS-BRL">ARS-BRL (Peso Argentino)</option>
+                  <option value="JPY-BRL">JPY-BRL (Iene Japonês)</option>
+                  <option value="CHF-BRL">CHF-BRL (Franco Suíço)</option>
+                  <option value="CNY-BRL">CNY-BRL (Yuan Chinês)</option>
+                  <option value="YLS-BRL">YLS-BRL (Novo Shekel Israelense)</option>
+                  <option value="BTC-BRL">BTC-BRL (Bitcoin)</option>
+                  <option value="LTC-BRL">LTC-BRL (Litecoin)</option>
+                  <option value="ETH-BRL">ETH-BRL (Ethereum)</option>
+                  <option value="XRP-BRL">XRP-BRL (Ripple)</option>
+                </select>
+              </Col>
+              <Col xs="auto">
+                <label>Data Inicial</label>
+                <FaRegCalendar /><DatePicker dateFormat="dd/MM/yyyy" className="mr-sm-2 form-control" selected={dataInicial} onChange={date => setDataInicial(date)} />
+              </Col>
+              <Col xs="auto">
+                <label>Data Final</label>
+                <FaRegCalendar /><DatePicker dateFormat="dd/MM/yyyy" className="mr-sm-2 form-control" selected={dataFinal} onChange={date => setDataFinal(date)} />
+              </Col>
+              <Col xs="auto">
+                <label>Indicador</label>
+                <select className="custom-select mr-sm-2" onChange={function (event) {
+                  setIndicador(event.target.value);
+                  setIndicadorTexto(event.target.value);
+                }}>
+                  {(indicadorTexto &&
+                    <option value="">{indicadorTexto}</option>)
+                    ||
+                    <option value="">Escolha uma opção</option>
+                  }
+                  <option value="bid" name="Valor de compra">Valor de compra</option>
+                  <option value="ask">Valor de venda</option>
+                  <option value="high">Valor Máximo</option>
+                  <option value="low">Valor Mínimo</option>
+                  <option value="pctChange">Variação %</option>
+                </select>
+              </Col>
+              <Col xs="auto"  >
+                <Button className="bg-success" onClick={() => GerarRelatorio()}>
+                  <FaSearchDollar />
+                Pesquisar
+                 </Button>
+              </Col>
+
+              {erroValidacaoCampos &&
+                <Toast onClose={() => setErroValidacaoCampos(null)} delay={4000} autohide className="bg-warning">
+                  <Toast.Header>
+                    <strong className="mr-auto">{erroValidacaoCampos}</strong>
+                    <small>😞</small>
+                  </Toast.Header>
+                  <Toast.Body className="bg-white text-warning">
+                    Por favor, faça uma nova busca.
+              </Toast.Body>
+                </Toast>
+              }
+
+              {erro &&
+                <Toast onClose={() => setErroValidacaoCampos(null)} delay={4000} autohide className="bg-danger">
+                  <Toast.Header>
+                    <strong className="mr-auto">{erro}</strong>
+                  </Toast.Header>
+                  <Toast.Body className="bg-white text-danger">
+                    Por favor, faça uma nova busca.
+              </Toast.Body>
+                </Toast>
+              }
+            </Form.Row>
+          </Form>
+        </Nav>
+      </>
+    )
+  }
+  /**
+   * Essa função é responsável por criar o rodapé do site
+   * @returns Rodapé do site
+   */
+  function Rodape() {
+    return (
+      <div className="rodape">
+        <MDBContainer className="container-rodape">
+          <table className="tabela-rodape">
+            <tbody>
+              <tr>
+                <td className="align-bottom celula-rodape">
+                  <strong> Developed by: Pedro Spinardi and Davi L. Carasilo -- 2021 A Fatec’s project </strong>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </MDBContainer>
+      </div>
+
+    )
+  }
+  /**
+   * Essa função é responsável por criar o corpo do site
+   * @returns Corpo do site
+   */
+  function Corpo() {
+    return (
+      <div className="corpo">
+      </div>
+    )
+  }
+
+
+
   return (
     <>
       <Cabecalho />
@@ -110,5 +217,6 @@ function App() {
     </>
   );
 }
+
 
 export default App;
